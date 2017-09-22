fd3b5ffe648d41b50de0d1cbe344d07a14862a63
martin <martin.ramos292@gmail.com>
2017-09-22 12:59:16 -0300

    Actualizo

--- DrunkenViking-master/images/buttons/README.txt
+++
@@ -1,4 +0,0 @@
-Derived from Pixel Planets
-by Master484
-http://opengameart.org/content/pixel-planets
-http://creativecommons.org/publicdomain/zero/1.0/
--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-    qunit = require('gulp-qunit');
-
-gulp.task('default', function() {
-    return gulp.src('./tests/unit/unittests_nupic-js.html')
-        .pipe(qunit());
-});
